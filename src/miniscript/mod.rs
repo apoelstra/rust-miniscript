026a7d8d22f0472f3957ca6d027bc18b3d7d2f2e
Andrew Poelstra <apoelstra@wpsoftware.net>
2025-05-18 17:21:18 +0000

    miniscript: replace decode_ext with decode_with_validation_params
    This moves us toward dropping ExtData.

--- src/miniscript/mod.rs
+++ src/miniscript/mod.rs
@@ -45,6 +45,7 @@ use crate::expression::{FromTree, TreeIterItem};
 use crate::miniscript::decode::Terminal;
 use crate::{
     expression, plan, Error, ForEachKey, FromStrKey, MiniscriptKey, ToPublicKey, Translator,
+    ValidationParams,
 };
 #[cfg(test)]
 mod ms_tests;
@@ -693,27 +694,27 @@ impl<Ctx: ScriptContext> Miniscript<Ctx::Key, Ctx> {
     /// embedded in the chain. While it is inadvisable to use insane Miniscripts,
     /// once it's on the chain you don't have much choice anymore.
     pub fn decode_consensus(script: &script::Script) -> Result<Miniscript<Ctx::Key, Ctx>, Error> {
-        Miniscript::decode_with_ext(script, &ExtParams::allow_all())
+        // FIXME by using ValidationParams::MAX we actually parse more than is allowed
+        // by consensus; in particular, we don't enforce size limits. If we want to do
+        // this, we need to add a new Ctx::CONSENSUS constant and use that.
+        Miniscript::decode_with_validation_params(script, &ValidationParams::MAX)
     }
 
     /// Attempt to decode a Miniscript from Script, specifying which validation parameters to apply.
-    pub fn decode_with_ext(
+    pub fn decode_with_validation_params(
         script: &script::Script,
-        ext: &ExtParams,
+        params: &ValidationParams,
     ) -> Result<Miniscript<Ctx::Key, Ctx>, Error> {
         let tokens = lex(script)?;
         let mut iter = TokenIter::new(tokens);
 
         let top = decode::decode(&mut iter)?;
         Ctx::check_global_validity(&top)?;
-        let type_check = types::Type::type_check(&top.node)?;
-        if type_check.corr.base != types::Base::B {
-            return Err(Error::NonTopLevel(format!("{:?}", top)));
-        };
+        types::Type::type_check(&top.node)?;
         if let Some(leading) = iter.next() {
             Err(Error::Trailing(leading.to_string()))
         } else {
-            top.ext_check(ext)?;
+            top.validate(params).map_err(Error::Validation)?;
             Ok(top)
         }
     }
@@ -751,7 +752,7 @@ impl<Ctx: ScriptContext> Miniscript<Ctx::Key, Ctx> {
     ///
     /// ```
     pub fn decode(script: &script::Script) -> Result<Miniscript<Ctx::Key, Ctx>, Error> {
-        let ms = Self::decode_with_ext(script, &ExtParams::sane())?;
+        let ms = Self::decode_with_validation_params(script, &Ctx::SANE)?;
         Ok(ms)
     }
 }
@@ -1845,7 +1846,7 @@ mod tests {
         let script = ms.encode();
         // The same test, but parsing from script
         SegwitMs::decode(&script).unwrap_err();
-        SegwitMs::decode_with_ext(&script, &ExtParams::insane()).unwrap_err();
+        SegwitMs::decode_with_validation_params(&script, &Segwitv0::CONSENSUS).unwrap_err();
         SegwitMs::decode_consensus(&script).unwrap();
 
         // Try replacing the raw_pkh with a pkh
