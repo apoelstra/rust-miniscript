7186bf7beac5e56f83a0d0acd76a1be4041d853f
Andrew Poelstra <apoelstra@wpsoftware.net>
2025-05-25 14:14:17 +0000

    remove AddrError and Secp from top-level Error enum
    Apparently these stopped being used at some point.

--- src/lib.rs
+++ src/lib.rs
@@ -433,16 +433,12 @@ pub trait ForEachKey<Pk: MiniscriptKey> {
 
 #[derive(Debug)]
 pub enum Error {
-    /// rust-bitcoin address error
-    AddrError(bitcoin::address::ParseError),
     /// rust-bitcoin p2sh address error
     AddrP2shError(bitcoin::address::P2shError),
     /// Could not satisfy a script (fragment) because of a missing signature
     MissingSig(bitcoin::PublicKey),
     /// General failure to satisfy
     CouldNotSatisfy,
-    /// Forward-secp related errors
-    Secp(bitcoin::secp256k1::Error),
     /// Bare descriptors don't have any addresses
     BareDescriptorAddr,
     /// No script code for Tr descriptors
@@ -466,11 +462,9 @@ const MAX_RECURSION_DEPTH: u32 = 402;
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            Error::AddrError(ref e) => fmt::Display::fmt(e, f),
             Error::AddrP2shError(ref e) => fmt::Display::fmt(e, f),
             Error::MissingSig(ref pk) => write!(f, "missing signature for key {:?}", pk),
             Error::CouldNotSatisfy => f.write_str("could not satisfy"),
-            Error::Secp(ref e) => fmt::Display::fmt(e, f),
             Error::BareDescriptorAddr => write!(f, "Bare descriptors don't have address"),
             Error::TrNoScriptCode => write!(f, "No script code for Tr descriptors"),
             Error::Threshold(ref e) => e.fmt(f),
@@ -487,24 +481,12 @@ impl std::error::Error for Error {
 
         match self {
             MissingSig(_) | CouldNotSatisfy | BareDescriptorAddr | TrNoScriptCode => None,
-            AddrError(e) => Some(e),
             AddrP2shError(e) => Some(e),
-            Secp(e) => Some(e),
             Threshold(e) => Some(e),
             Parse(e) => Some(e),
             Validation(e) => Some(e),
         }
     }
-}
-
-#[doc(hidden)]
-impl From<bitcoin::secp256k1::Error> for Error {
-    fn from(e: bitcoin::secp256k1::Error) -> Error { Error::Secp(e) }
-}
-
-#[doc(hidden)]
-impl From<bitcoin::address::ParseError> for Error {
-    fn from(e: bitcoin::address::ParseError) -> Error { Error::AddrError(e) }
 }
 
 #[doc(hidden)]
